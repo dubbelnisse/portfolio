c20b0a623c37b074180becb7e15c1e4ecd70d814
Nils Nilsson <nilspontusnilsson@gmail.com>
2020-03-30 13:19:19 +0200

    feat: add back button

--- src/routes/projects/Iteam.tsx
+++ src/routes/projects/Iteam.tsx
@@ -2,14 +2,21 @@ import React from 'react'
 import projects from '../../data/iteam-projects'
 import Project from '../../components/Project'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 const Wrapper = styled.div`
   max-width: 600px;
 `
 
+const NavigateBack = styled(Link)`
+  display: block;
+  margin: 20px 0;
+`
+
 const Iteam = () => {
   return (
     <Wrapper>
+      <NavigateBack to="/">&#8592; Back</NavigateBack>
       <h1>Projects @ Iteam</h1>
       {projects.map((project, i) => (
         <Project key={`project-${i}`} project={project} />
